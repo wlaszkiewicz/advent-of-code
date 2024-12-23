719a1149bcfe415ba723ea2fc7e5c9468369b53e
wlaszkiewicz <nika25.laszkiewicz@gmail.com>
2024-12-23 21:38:17 +0100

    day 1 part 2 essa

--- app/src/main/java/com/example/advent/day1/Day1.kt
+++ app/src/main/java/com/example/advent/day1/Day1.kt
@@ -4,17 +4,41 @@ import java.io.File
 
 fun main() {
     val input = readFile("app/src/main/java/com/example/advent/day1/day1.txt")
-    val list1 = mutableListOf<String>()
-    val list2 = mutableListOf<String>()
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+
     val numLines = input.lines().size
     addToList1(input, list1, numLines)
     addToList2(input, list2, numLines)
 
-    val totalDistance = getTotalDistance(list1, list2)
-    println("The total distance is: $totalDistance")
+    println(part1(list1, list2))
+
+    addToList1(input, list1, numLines)
+    addToList2(input, list2, numLines)
+
+    println(part2(list1, list2))
+
+
 }
 
-private fun getTotalDistance(list1: MutableList<String>, list2: MutableList<String>): Int {
+private fun part2(list1: MutableList<Int>, list2: MutableList<Int>): String {
+    var similarity = 0
+    for (i in 0 until list1.size) {
+        val num = list1[i]
+        var countNum = 0
+        for (j in 0 until list2.size) {
+            val num2 = list2[j]
+            if (num == num2) {
+                countNum += 1
+            }
+        }
+        similarity += countNum * num
+    }
+    return "The similarity is: $similarity"
+}
+
+
+private fun part1(list1: MutableList<Int>, list2: MutableList<Int>): String {
     var totalDistance = 0
     for (i in 0 until list1.size) {
         val smallestNum1 = list1.minOrNull()
@@ -22,34 +46,21 @@ private fun getTotalDistance(list1: MutableList<String>, list2: MutableList<Stri
         val smallestNum2 = list2.minOrNull()
         list2.remove(smallestNum2)
 
-        totalDistance += findDistance(smallestNum1, smallestNum2)
-    }
-    return totalDistance
-}
-
-fun findDistance(smallestNum1: String?, smallestNum2: String?): Int {
-    var distance = 0
-    if (smallestNum1 != null && smallestNum2 != null) {
-        if (smallestNum1.toInt() > smallestNum2.toInt()) {
-            distance = smallestNum1.toInt() - smallestNum2.toInt()
-        } else {
-            distance = smallestNum2.toInt() - smallestNum1.toInt()
+        if (smallestNum2 != null && smallestNum1 != null) {
+        totalDistance += kotlin.math.abs(smallestNum1 - smallestNum2)
         }
-    } else {
-        throw Exception("One of the numbers is null")
     }
-    return distance
-
+    return "The total distance is: $totalDistance"
 }
 
-fun addToList1(input: String, list: MutableList<String>, numLines: Int) {
-    for (i in 0 until numLines) {  // iterate through the lines
-        list.add(input.lines()[i].substringBefore(" ").trim())  // add the first number to the list and remove any leading or trailing whitespace
-    }
-}
-fun addToList2(input: String, list: MutableList<String>, numLines: Int) {
+fun addToList1(input: String, list: MutableList<Int>, numLines: Int) {
     for (i in 0 until numLines) {
-        list.add(input.lines()[i].substringAfter(" ").trim())  // add the second number to the list
+        list.add(input.lines()[i].substringBefore(" ").trim().toInt())  // add the first number to the list and remove any leading or trailing whitespace
+    }
+}
+fun addToList2(input: String, list: MutableList<Int>, numLines: Int) {
+    for (i in 0 until numLines) {
+        list.add(input.lines()[i].substringAfter(" ").trim().toInt())  // add the second number to the list
     }
 }
 
