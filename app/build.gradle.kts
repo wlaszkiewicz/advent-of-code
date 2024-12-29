3de1f5efa9f122c70bb28a89ded8013450a0cfa3
wlaszkiewicz <nika25.laszkiewicz@gmail.com>
2024-12-29 22:36:13 +0100

    day 9 (nie rozmawiamy o 8 atm ok)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
